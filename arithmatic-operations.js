6f168a7f5e18b7ab6ae2e8bd74efedb47bd49bfc
flash_hammad <flash_hammad@yahoo.co.in>
2022-08-08 08:16:45 +0530

    Javascript Session 2

---
+++ arithmatic-operations.js
@@ -0,0 +1,18 @@
+// var num1 = prompt("test");
+
+// console.log( parseInt(num1,10));
+
+var number1 = 40;
+var number2 = 58;
+
+var resultAdd = number1 + number2;
+var resultSub = number1 - number2;
+var resultMul = number1 * number2;
+var resultPow = Math.pow(number1, number2)
+
+console.log("Sum of " + number1 + "  " + number2 + " is " + resultAdd);
+console.log("Subtract of " + number1 + "  " + number2 + " is " + resultSub);
+console.log("resultMul of " + number1 + "  " + number2 + " is " + resultMul);
+console.log("resultPow of " + number1 + "  " + number2 + " is " + resultPow);
+
+
