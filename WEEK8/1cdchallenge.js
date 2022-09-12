470a4eb1978d2188c3ac79fa0ad7338840a4b27f
flash_hammad <flash_hammad@yahoo.co.in>
2022-09-12 22:29:20 +0530

    week 8 coding solution 1

---
+++ WEEK8/1cdchallenge.js
@@ -0,0 +1,36 @@
+// Write a function called displayName() that takes an object as an argument and 
+//prints the person's first and last name. Use object destructuring in the function argument. 
+//And also, use template strings when printing the first and last name.
+
+// Ex object is-
+
+ 
+
+//  const person = {
+
+//       first: 'Virat',
+
+//       last: 'Kohli',
+
+//       twitter: '@vkohli',
+
+//       company: 'Cricketer'
+
+//   }
+
+//----------------------------
+
+const person = {
+    first : 'Virat',
+    last : 'Kholi',
+    twitter: '@vkohli',
+    company: 'Cricketer'
+}
+
+function displayName( first, last) {
+    // console.log(first + " --> " + last);
+    console.log(`${first} --> ${last}`);
+}
+
+const { first, last } = person;
+displayName(first, last);
