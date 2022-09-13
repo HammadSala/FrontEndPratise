b08bf443e5404f562d488c4a28fb06af305484ae
flash_hammad <flash_hammad@yahoo.co.in>
2022-09-13 07:33:37 +0530

    13-Sep js chalenge 3

---
+++ WEEK8/3cdchallenge.js
@@ -0,0 +1,32 @@
+// Create a class having one constructor and a method named age.
+
+// The constructor accepts two arguments: one is the name of the car and second is the manufacturing year of that car.
+
+// The age() method accepts an argument which is the year in which we want to find the age of that car.ie, 
+// if the argument is 2022 and the year of car manufacturing is 2014 so age() will return 2022-2014=8.
+
+ 
+
+// Now create the object of the above class and set the name and manufacturing year of the car as “Ford”,2012 
+// while object creation.
+
+// Now get the current year from the date object in js and print the age of the car.
+
+class Car {
+    constructor(name, make) {
+        this.name= name;
+        this.make = make;
+
+    }
+
+    age(){
+        console.log(new Date().getFullYear());
+        var age = new Date().getUTCFullYear() - this.make;
+        console.log("Age of the car is :" + age);
+
+    }
+}
+
+const obj1 = new Car("ford", 2012)
+
+obj1.age();
